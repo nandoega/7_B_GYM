b8fa948916b3096f8687e53f8590602516d4d88a
nandoega <220711970@students.uajy.ac.id>
2024-10-02 15:38:52 +0700

    fix main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pbp/View/login.dart'; // Import corrected
 
 void main() {
   runApp(const MainApp());
@@ -10,11 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('test'),
-        ),
-      ),
+      home: LoginView(), // Fixed: added missing comma
     );
   }
 }
