d84825c3e7e1a64ade7c023e75f1ab2a894a4769
nandoega <220711970@students.uajy.ac.id>
2024-11-15 16:26:32 +0700

    pulled a file from admin page

---
+++ lib/View/admin.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class AtmaGymPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Container(
+          width: 405,
+          decoration: BoxDecoration(
+            color: Color(0xFF2A2A3A),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          padding: EdgeInsets.symmetric(vertical: 20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Atma Gym',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 20),
+              GridView.count(
+                crossAxisCount: 2,
+                shrinkWrap: true,
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                children: [
+                  _buildButton('User', Colors.blue),
+                  _buildButton('Create Workout', Colors.blue),
+                  _buildButton('Sport Class', Colors.yellow),
+                  _buildButton('Equipment', Colors.yellow),
+                  _buildButton('Personal Trainer', Colors.yellow),
+                  _buildButton('Schedule', Colors.red),
+                  _buildButton('Review', Colors.white),
+                  _buildButton('Transaction', Colors.white),
+                ],
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // Add logout functionality here
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                ),
+                child: Text('Log Out', style: TextStyle(fontSize: 16)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildButton(String text, Color color) {
+    return Container(
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black26,
+            offset: Offset(0, 4),
+            blurRadius: 4,
+          ),
+        ],
+      ),
+      child: Center(
+        child: Text(
+          text,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: color == Colors.white ? Colors.black : Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
