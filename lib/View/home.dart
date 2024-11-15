415b49d9881fe904e8954ed9e1298d99828a27f1
Adistyo-Alexia <220712028@students.uajy.ac.id>
2024-11-15 17:26:26 +0700

    first commit profile page

--- lib/View/home.dart
+++ lib/View/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:pbp/View/view_list.dart'; // ListNamaView import
+import 'package:pbp/View/view_list.dart'; // Import ListNamaView if it's part of your project
+import 'profile_view.dart'; // Import ProfileView
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -21,37 +22,75 @@ class _HomeViewState extends State<HomeView> {
     Center(
       child: Image(image: NetworkImage('https://picsum.photos/200/300')),
     ),
-    ListNamaView(), // Halaman List dengan 3 tombol
-    Center(
-      child: Text(
-        'Index 2: Profile',
-        style: TextStyle(fontSize: 20),
-      ),
+    ListNamaView(), // Replace with your actual list view widget
+    ProfileView(
+      data: {
+        'name': 'Aiko Evelyn',
+        'email': 'aikoevelyn@gmail.com',
+        'profileImage':
+            'https://example.com/profile.jpg', // Replace with actual image URL
+      },
     ),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Beranda',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list),
-            label: 'List',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profil',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-      ),
+      backgroundColor:
+          Color(0xFF2C2B5E), // Set the background color of the main screen
       body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: Container(
+        margin: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.circular(30),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              offset: Offset(0, 4),
+              blurRadius: 10,
+              spreadRadius: 1,
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+          child: BottomNavigationBar(
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home,
+                  color: _selectedIndex == 0 ? Colors.white : Colors.black,
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.person,
+                  color: _selectedIndex == 1 ? Colors.white : Colors.black,
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.settings,
+                  color: _selectedIndex == 2 ? Colors.white : Colors.black,
+                ),
+                label: '',
+              ),
+            ],
+            currentIndex: _selectedIndex,
+            onTap: _onItemTapped,
+            backgroundColor:
+                Colors.transparent, // Transparent to show container color
+            selectedItemColor: Colors.white,
+            unselectedItemColor: Colors.black,
+            showSelectedLabels: false,
+            showUnselectedLabels: false,
+            type: BottomNavigationBarType.fixed,
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/View/profile_view.dart
+++ lib/View/profile_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'payment.dart'; // Import the PaymentScreen
+import 'login.dart'; // Import the LoginView
 
 class ProfileView extends StatelessWidget {
   final Map<String, dynamic> data;
@@ -8,28 +10,193 @@ class ProfileView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFF2C2B5E),
       appBar: AppBar(
         title: const Text('Profile'),
+        backgroundColor: Color(0xFF2C2B5E),
+        elevation: 0,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Name: ${data['name']}', style: const TextStyle(fontSize: 18)),
-            const SizedBox(height: 10),
-            Text('Username: ${data['username']}', style: const TextStyle(fontSize: 18)),
-            const SizedBox(height: 10),
-            Text('Email: ${data['email']}', style: const TextStyle(fontSize: 18)),
-            const SizedBox(height: 10),
-            Text('Phone Number: ${data['notelp']}', style: const TextStyle(fontSize: 18)),
-            const SizedBox(height: 10),
-            Text('Date of Birth: ${data['dob']}', style: const TextStyle(fontSize: 18)),
-            const SizedBox(height: 10),
-            Text('Address: ${data['address']}', style: const TextStyle(fontSize: 18)),
-          ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Profile Card
+              Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16.0),
+                ),
+                elevation: 5,
+                child: Container(
+                  width: double.infinity, // Makes the card take full width
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 20.0, horizontal: 16.0),
+                  child: Column(
+                    children: [
+                      Stack(
+                        alignment: Alignment.topRight,
+                        children: [
+                          CircleAvatar(
+                            backgroundImage:
+                                NetworkImage(data['profileImage'] ?? ''),
+                            radius: 50,
+                          ),
+                          Positioned(
+                            top: 5,
+                            right: 5,
+                            child: Icon(
+                              Icons.edit,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        data['name'] ?? 'Name',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Text(
+                        data['email'] ?? 'email@example.com',
+                        style: TextStyle(
+                          color: Colors.grey[600],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+
+              // Icon Buttons (History, Payment, Schedule)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _buildFeatureButton(context, Icons.history, 'History', () {}),
+                  _buildFeatureButton(
+                    context,
+                    Icons.payment,
+                    'Payment',
+                    () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => PaymentScreen()),
+                      );
+                    },
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _buildFeatureButton(
+                      context, Icons.schedule, 'Schedule', () {}),
+                ],
+              ),
+              SizedBox(height: 30),
+
+              // Action Buttons (Log Out and Delete Account)
+              _buildActionButton(context, 'Log Out', Colors.red, () {
+                _showLogoutDialog(context);
+              }),
+              SizedBox(height: 10),
+              _buildActionButton(
+                  context, 'Delete Account', Colors.redAccent, () {}),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  Widget _buildFeatureButton(
+      BuildContext context, IconData icon, String label, VoidCallback onTap) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        children: [
+          Container(
+            width: 120,
+            height: 120,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  blurRadius: 5,
+                  offset: Offset(2, 2),
+                ),
+              ],
+            ),
+            child: Center(
+              child: Icon(icon, size: 50, color: Colors.black),
+            ),
+          ),
+          SizedBox(height: 8),
+          Text(
+            label,
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildActionButton(
+      BuildContext context, String label, Color color, VoidCallback onTap) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: onTap,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: color,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+          padding: EdgeInsets.symmetric(vertical: 16),
+        ),
+        child: Text(
+          label,
+          style: TextStyle(color: Colors.white, fontSize: 16),
+        ),
+      ),
+    );
+  }
+
+  void _showLogoutDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Log Out"),
+          content: Text("Are you sure you want to log out?"),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Close the dialog
+              },
+              child: Text("No"),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginView()),
+                );
+              },
+              child: Text("Yes"),
+            ),
+          ],
+        );
+      },
     );
   }
 }
